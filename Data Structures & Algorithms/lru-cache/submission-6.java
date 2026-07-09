40244739aa2c7ca5557b02186d3894768de3f381
Mohamed Nushath <81402386+nujaadeen@users.noreply.github.com>
2026-07-09 18:46:14 +0400

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.java
@@ -0,0 +1,85 @@
+class LRUCache {
+    Map<Integer, Node> lruCache;
+    int capacity;
+    Node head;
+    Node tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        lruCache = HashMap.newHashMap(capacity);
+    }
+    
+    public int get(int key) {
+        Node node = lruCache.get(key);
+
+        if (node == null) return -1;
+        moveToTail(node);
+
+        return node.val;
+    }
+    
+    public void put(int key, int value) {
+        Node foundNode = lruCache.get(key);
+
+        if (foundNode == null) {
+            Node newNode = new Node(key, value, tail, null);
+            lruCache.put(key, newNode);
+
+            if (lruCache.size() == 1) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                newNode.prev = tail;
+                tail = newNode;
+            } 
+        } else {
+            moveToTail(foundNode);
+            foundNode.val = value;
+        }
+        
+        if (lruCache.size() > capacity) {
+            lruCache.remove(head.key);
+
+            head = head.next;
+
+            if (head != null) {
+                head.prev = null;
+            } else {
+                tail = null;
+            }
+        }
+        
+    }
+
+    private void moveToTail(Node node) {
+        if (node == tail) return;
+
+        if (node == head) {
+            head = node.next;
+            head.prev = null;
+        } else {
+            node.next.prev = node.prev;
+            node.prev.next = node.next;
+        }
+
+        tail.next = node;
+        node.prev = tail;
+        node.next = null;
+        tail = node;
+    }
+}
+
+class Node {
+    int key;
+    int val;
+    Node prev;
+    Node next;
+
+    public Node(int key, int val, Node prev, Node next) {
+        this.key = key;
+        this.val = val;
+        this.prev = prev;
+        this.next = next;
+    }
+}
